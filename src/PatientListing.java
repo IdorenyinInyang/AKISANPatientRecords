de90cc4f9c0458b4fc675815ac78dc66c8940ccb
IdorenyinInyang <iainyang@aggies.ncat.edu>
2018-08-16 19:02:47 -0400

    PatientListing class created
    PatientListing is to hold the objects of patient information.

---
+++ src/PatientListing.java
@@ -0,0 +1,11 @@
+import java.util.*; 
+import java.io.*;
+
+public class PatientListing { 
+
+	public static HashMap<Integer, PatientInfo> patientList = new HashMap<Integer, PatientInfo>(); 
+	
+	public static void main(String args[]) {
+		
+	} 
+}
