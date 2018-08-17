78f082e4b74d6fd9b209dbf8d9eb44bd4246640d
IdorenyinInyang <iainyang@aggies.ncat.edu>
2018-08-16 20:53:16 -0400

    Removed main method
    Program will not run from here, so main method is unnecessary.

--- src/PatientListing.java
+++ src/PatientListing.java
@@ -4,11 +4,7 @@ import java.io.*;
 public class PatientListing { 
 
 	public static HashMap<Integer, PatientInfo> patientList = new HashMap<Integer, PatientInfo>(); 
-	
-	public static void main(String args[]) {
 		
-	}  
-	
 	public static void addPatient(PatientInfo patient, int patientID) { 
 		patientList.put(patientID, patient);
 	} 
