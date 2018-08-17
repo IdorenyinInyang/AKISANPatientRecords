d7872452a1317f3f192e7c1cfbe600b0ae899e25
IdorenyinInyang <iainyang@aggies.ncat.edu>
2018-08-16 20:52:35 -0400

    Added methods for PatientListing
    Method addPatient will add a patient object to the patientList HashMap, removePatient will remove the patient object from the HashMap, and getPatient method will return a patient object.

--- src/PatientListing.java
+++ src/PatientListing.java
@@ -7,5 +7,17 @@ public class PatientListing {
 	
 	public static void main(String args[]) {
 		
+	}  
+	
+	public static void addPatient(PatientInfo patient, int patientID) { 
+		patientList.put(patientID, patient);
 	} 
+	
+	public static void removePatient(PatientInfo patient, int patientID) { 
+		patientList.put(patientID, patient);
+	} 
+	
+	public static PatientInfo getPatient(int patientID) { 
+		return patientList.get(patientID);
+	}
 }
